7e861e6e6b71944aa6b7c0257f0e19e52df4cbfc
kolobkevic <kolobkevic@google.com>
2023-08-22 12:03:56 +0300

    Added Minio

---
+++ src/main/java/ru/kolobkevic/cloud_storage/configs/MinioConfig.java
@@ -0,0 +1,53 @@
+package ru.kolobkevic.cloud_storage.configs;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.errors.ErrorResponseException;
+import io.minio.errors.InsufficientDataException;
+import io.minio.errors.InternalException;
+import io.minio.errors.InvalidResponseException;
+import io.minio.errors.ServerException;
+import io.minio.errors.XmlParserException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+@Configuration
+public class MinioConfig {
+    @Value("${spring.minio.url}")
+    private String url;
+    @Value("${spring.minio.access-key}")
+    private String accessKey;
+    @Value("${spring.minio.secret-key}")
+    private String secretKey;
+    @Value("${spring.minio.port}")
+    private int port;
+    @Value("${spring.minio.bucket")
+    private String bucketName;
+
+    @Bean
+    public MinioClient getMinioClient() {
+        return MinioClient.builder()
+                .endpoint(url, port, false)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+
+    @Bean
+    public void createBucketIfNotExists() throws
+            ServerException, InsufficientDataException, ErrorResponseException,
+            IOException, NoSuchAlgorithmException, InvalidKeyException,
+            InvalidResponseException, XmlParserException, InternalException {
+
+        MinioClient minioClient = getMinioClient();
+
+        if (!minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build())) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+        }
+    }
+}
