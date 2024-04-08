e822a3ce71e182b75fc3c2ab13867a7085c6dd33
Zaur Bogdaev <kolobkevic@gmail.com>
2024-04-08 19:12:02 +0300

    Исправил наследование классов от Exception на RuntimeException

--- src/main/java/ru/kolobkevic/cloud_storage/exceptions/ObjectAlreadyExistsException.java
+++ src/main/java/ru/kolobkevic/cloud_storage/exceptions/ObjectAlreadyExistsException.java
@@ -1,6 +1,6 @@
 package ru.kolobkevic.cloud_storage.exceptions;
 
-public class ObjectAlreadyExistsException extends Exception {
+public class ObjectAlreadyExistsException extends RuntimeException {
     public ObjectAlreadyExistsException(String message) {
         super(message);
     }
--- src/main/java/ru/kolobkevic/cloud_storage/exceptions/StorageServerException.java
+++ src/main/java/ru/kolobkevic/cloud_storage/exceptions/StorageServerException.java
@@ -1,6 +1,6 @@
 package ru.kolobkevic.cloud_storage.exceptions;
 
-public class StorageServerException extends Exception{
+public class StorageServerException extends RuntimeException{
     public StorageServerException(String message) {
         super(message);
     }
