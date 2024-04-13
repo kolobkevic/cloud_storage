936aefeb94daef72c1eec7195eaffa46ea22d3d1
Zaur Bogdaev <kolobkevic@gmail.com>
2024-04-13 09:15:58 +0300

    Добавил класс MinioProperties

--- src/main/java/ru/kolobkevic/cloud_storage/configs/MinioConfig.java
+++ src/main/java/ru/kolobkevic/cloud_storage/configs/MinioConfig.java
@@ -3,52 +3,30 @@ package ru.kolobkevic.cloud_storage.configs;
 import io.minio.BucketExistsArgs;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
-import io.minio.errors.ErrorResponseException;
-import io.minio.errors.InsufficientDataException;
-import io.minio.errors.InternalException;
-import io.minio.errors.InvalidResponseException;
-import io.minio.errors.ServerException;
-import io.minio.errors.XmlParserException;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
 @Configuration
+@RequiredArgsConstructor
 public class MinioConfig {
-    @Value("${minio.url}")
-    private String url;
-    @Value("${minio.access-key}")
-    private String accessKey;
-    @Value("${minio.secret-key}")
-    private String secretKey;
-    @Value("${minio.port}")
-    private int port;
-    @Value("${minio.bucket}")
-    private String bucketName;
+    private final MinioProperties minioProperties;
+
 
     @Bean
-    public MinioClient getMinioClient() throws ServerException, InsufficientDataException,
-            ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException,
-            InvalidResponseException, XmlParserException, InternalException {
+    public MinioClient getMinioClient() throws Exception {
         MinioClient minioClient = MinioClient.builder()
-                .endpoint(url, port, false)
-                .credentials(accessKey, secretKey)
+                .endpoint(minioProperties.getUrl(), minioProperties.getPort(), false)
+                .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
                 .build();
         createBucketIfNotExists(minioClient);
         return minioClient;
     }
 
-    public void createBucketIfNotExists(MinioClient minioClient) throws
-            ServerException, InsufficientDataException, ErrorResponseException,
-            IOException, NoSuchAlgorithmException, InvalidKeyException,
-            InvalidResponseException, XmlParserException, InternalException {
+    public void createBucketIfNotExists(MinioClient minioClient) throws Exception{
 
-        if (!minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build())) {
-            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+        if (!minioClient.bucketExists(BucketExistsArgs.builder().bucket(minioProperties.getBucket()).build())) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(minioProperties.getBucket()).build());
         }
     }
 }
